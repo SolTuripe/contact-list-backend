e6fa07a62c1bec502c29cc8841366c4d5bd97152
Sol Turipe <solcis.bio@gmail.com>
2022-11-16 18:21:02 +0100

    minor refactor

--- src/main/java/com/contactlist/fullstackbackend/model/User.java
+++ src/main/java/com/contactlist/fullstackbackend/model/User.java
@@ -1,12 +1,16 @@
 package com.contactlist.fullstackbackend.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Setter
+@Getter
 @Entity
 public class User {
-
     @Id
     @GeneratedValue
     private Long id;
@@ -14,44 +18,4 @@ public class User {
     private String lastName;
     private String email;
     private Long phoneNumber;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
